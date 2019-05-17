9351b390ff909009416b6e19c8f73bdebaf3caba
Rizwan Saeed <rizwan.saeed@finbourne.com>
2019-05-17 11:51:55 +0100

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -9,7 +9,16 @@ if [[ (${#1} -eq 0) ]] ; then
     exit 1
 fi
 
-NUGET_API_KEY=$1
+if [[ (${#2} -eq 0) ]] ; then
+    echo 
+    echo "[ERROR] missing NuGet repo"
+    echo
+    exit 1
+fi
+
+nuget_api_key=$1
+nuget_source=$2
+
 sdk_version=$(cat lusid.json | jq -r '.info.version')
 
 sed -i 's/<Version>.*<\/Version>/<Version>'$sdk_version'<\/Version>/g' sdk/Lusid.Sdk/Lusid.Sdk.csproj
@@ -18,5 +27,5 @@ echo "sdk_version=$sdk_version"
 
 dotnet pack -c Release sdk/Lusid.Sdk/Lusid.Sdk.csproj
 dotnet nuget push sdk/Lusid.Sdk/bin/Release/Lusid.Sdk.${sdk_version}.nupkg \
-    --source https://nuget.org \
-    --api-key $NUGET_API_KEY
+    --source $nuget_source \
+    --api-key $nuget_api_key
