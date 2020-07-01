abb0bdd81ea23a3ceee4dfb3adab2594111fd587
Stephen LM <engineering@finbourne.com>
2020-07-01 15:21:06 +0100

    add constituent port tests

--- sdk/Lusid.Sdk.Tests/tutorials/Ibor/ReferencePorfolios.cs
+++ sdk/Lusid.Sdk.Tests/tutorials/Ibor/ReferencePorfolios.cs
@@ -11,7 +11,7 @@ namespace Lusid.Sdk.Tests.Tutorials.Ibor
 {
 
     [TestFixture]
-    public class ReferencePortfolios
+    public class ReferencePortfolioTests
     {
         private ILusidApiFactory _apiFactory;
         private IList<string> _instrumentIds;
@@ -21,7 +21,6 @@ namespace Lusid.Sdk.Tests.Tutorials.Ibor
         const string TutorialScope = "Testdemo1";
 
         [OneTimeSetUp]
-
         public void SetUp()
         {
             //  Initialise the API factory
@@ -30,16 +29,13 @@ namespace Lusid.Sdk.Tests.Tutorials.Ibor
             // Load default instruments which will be used in the tests
             var instrumentsLoader = new InstrumentLoader(_apiFactory);
             _instrumentIds = instrumentsLoader.LoadInstruments().OrderBy(x => x).ToList();
-
         }
 
         [Test]
-
         public void CreateReferencePortfolio()
-
         {
             //  Create date for the portfolio
-            var create_date = new DateTimeOffset(2000, 1, 1, 0, 0, 0, TimeSpan.Zero);
+            var createDate = new DateTimeOffset(2000, 1, 1, 0, 0, 0, TimeSpan.Zero);
 
             //  Details of the new reference portfolio to be created
             var request = new CreateReferencePortfolioRequest(
@@ -51,7 +47,7 @@ namespace Lusid.Sdk.Tests.Tutorials.Ibor
                 displayName: $"Portfolio-{PorfolioCode}",
 
                 //  Create date for the below. We can only upsert constituents after this date.
-                created: create_date
+                created: createDate
 
             );
 
@@ -64,10 +60,8 @@ namespace Lusid.Sdk.Tests.Tutorials.Ibor
         }
 
         [Test]
-
         public void SetConstituents()
         {
-
             // Declare some instruments which we loaded earlier
             var instrument1 = _instrumentIds[0];
             var instrument2 = _instrumentIds[1];
@@ -80,7 +74,7 @@ namespace Lusid.Sdk.Tests.Tutorials.Ibor
             var weighting = UpsertReferencePortfolioConstituentsRequest.WeightTypeEnum.Static;
 
             // Create the request to upsert constituents
-            var UpsertConstituentsRequest = new UpsertReferencePortfolioConstituentsRequest(
+            var upsertConstituentsRequest = new UpsertReferencePortfolioConstituentsRequest(
 
                 effectiveFrom: date,
                 weightType: weighting,
@@ -98,7 +92,6 @@ namespace Lusid.Sdk.Tests.Tutorials.Ibor
                         weight: 20,
                         currency: "GBP"
                     ),
-
                      new ReferencePortfolioConstituentRequest(
                         instrumentIdentifiers: new Dictionary<string, string> {[TestDataUtilities.LusidInstrumentIdentifier] = instrument2},
                         weight: 30,
@@ -112,32 +105,27 @@ namespace Lusid.Sdk.Tests.Tutorials.Ibor
                     ),
                 }
             );
-
             // Upsert the constituents into LUSID
-            var SetConstituents = _apiFactory.Api<IReferencePortfolioApi>().UpsertReferencePortfolioConstituents(TutorialScope, PorfolioCode, UpsertConstituentsRequest);
+            var setConstituents = _apiFactory.Api<IReferencePortfolioApi>().UpsertReferencePortfolioConstituents(TutorialScope, PorfolioCode, upsertConstituentsRequest);
 
-            var RefHoldings = _apiFactory.Api<IReferencePortfolioApi>().GetReferencePortfolioConstituents(TutorialScope, PorfolioCode, date);
+            var constituentHoldings = _apiFactory.Api<IReferencePortfolioApi>().GetReferencePortfolioConstituents(TutorialScope, PorfolioCode, date);
 
-            Assert.That(RefHoldings.Constituents.Count(), Is.EqualTo(4));
+            // Check the count of holdings
 
-            RefHoldings.Constituents.Sort((h1, h2) => String.Compare(h1.InstrumentUid, h2.InstrumentUid, StringComparison.Ordinal));
+            Assert.That(constituentHoldings.Constituents.Count(), Is.EqualTo(4));
 
-            Assert.That(RefHoldings.Constituents[1].InstrumentUid, Is.EqualTo(_instrumentIds[0]));
-            Assert.That(RefHoldings.Constituents[2].InstrumentUid, Is.EqualTo(_instrumentIds[1]));
-            Assert.That(RefHoldings.Constituents[3].InstrumentUid, Is.EqualTo(_instrumentIds[2]));
+            // Check the instruments on the holdings
+            constituentHoldings.Constituents.Sort((h1, h2) => String.Compare(h1.InstrumentUid, h2.InstrumentUid, StringComparison.Ordinal));
 
+            Assert.That(constituentHoldings.Constituents[1].InstrumentUid, Is.EqualTo(_instrumentIds[0]));
+            Assert.That(constituentHoldings.Constituents[2].InstrumentUid, Is.EqualTo(_instrumentIds[1]));
+            Assert.That(constituentHoldings.Constituents[3].InstrumentUid, Is.EqualTo(_instrumentIds[2]));
 
-            //    Validate the holdings
-
-            Assert.That(RefHoldings.Constituents[0].Weight, Is.EqualTo(10));
-            Assert.That(RefHoldings.Constituents[1].Weight, Is.EqualTo(20));
-            Assert.That(RefHoldings.Constituents[2].Weight, Is.EqualTo(30));
-            Assert.That(RefHoldings.Constituents[3].Weight, Is.EqualTo(40));
-
-
+            //  Validate the holding weights
+            Assert.That(constituentHoldings.Constituents[0].Weight, Is.EqualTo(10));
+            Assert.That(constituentHoldings.Constituents[1].Weight, Is.EqualTo(20));
+            Assert.That(constituentHoldings.Constituents[2].Weight, Is.EqualTo(30));
+            Assert.That(constituentHoldings.Constituents[3].Weight, Is.EqualTo(40));
         }
-
-
-
     }
 }
