5998ba4217540925e09822029c1110b676ddcaa7
Rizwan Saeed <rizwan.saeed@finbourne.com>
2020-11-25 16:02:48 +0000

    SE-542 Account for hex encoding of value

--- sdk/Lusid.Sdk.Tests/LusidApiFactoryTests.cs
+++ sdk/Lusid.Sdk.Tests/LusidApiFactoryTests.cs
@@ -13,7 +13,7 @@ namespace Lusid.Sdk.Tests
     public class LusidApiFactoryTests
     {
         private ILusidApiFactory _factory = new LusidApiFactory(new Configuration());
-        private const string RequestIdRegexPattern = "[a-zA-Z0-9]{13}:[0-9]{8}";
+        private const string RequestIdRegexPattern = "[a-zA-Z0-9]{13}:[0-9a-fA-F]{8}";
 
         [OneTimeSetUp]
         public void SetUp()
