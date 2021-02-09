9d8a0faaab6b727f83213128b579061c886a348e
Michael McGarry <mike.mcgarry@finbourne.com>
2021-02-09 05:36:48 +0000

    WEAL-1160 Added request data extractor utility to C# SDK, already in preview SDK

--- sdk/Lusid.Sdk/Utilities/ApiResponseExtensions.cs
+++ sdk/Lusid.Sdk/Utilities/ApiResponseExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Lusid.Sdk.Client;
 
 namespace Lusid.Sdk.Utilities
@@ -13,12 +14,30 @@ namespace Lusid.Sdk.Utilities
         public const string RequestIdHeader = "lusid-meta-requestId";
         
         /// <summary>
+        /// The header that the LUSID Date Time Offset is contained within.
+        /// </summary>
+        public const string DateHeader = "Date";
+        
+        /// <summary>
         /// Extracts the requestId from an ApiResponse
         /// </summary>
         public static string GetRequestId<T>(this ApiResponse<T> apiResponse)
         {
             // Extract requestId from Insights link contained in the Instance property
             return apiResponse.Headers.ContainsKey(RequestIdHeader) ? apiResponse.Headers[RequestIdHeader] : null;
+        }
+        
+        /// <summary>
+        /// Extracts the Date from an ApiResponse
+        /// </summary>
+        public static DateTimeOffset? GetRequestDateTime <T>(this ApiResponse<T> apiResponse)
+        {
+            if (!apiResponse.Headers.ContainsKey(DateHeader) ||
+                !DateTimeOffset.TryParse(apiResponse.Headers[DateHeader], out var headerDateValue))
+            {
+                return null;
+            }
+            return headerDateValue;
         }
     }
 }
