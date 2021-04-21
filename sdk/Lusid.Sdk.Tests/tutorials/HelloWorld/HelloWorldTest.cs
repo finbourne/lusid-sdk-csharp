7e3c72dff7c55e836ab41bbd30d7b613b0828661
Mark Needham <m.h.needham@gmail.com>
2021-04-21 15:09:28 +0100

    SE-1150: Hello World (#95)
    Co-authored-by: mneedham <mark.needham@finbourne.com>

---
+++ sdk/Lusid.Sdk.Tests/tutorials/HelloWorld/HelloWorldTest.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace Lusid.Sdk.Tests.tutorials.HelloWorld
+{
+    [TestFixture]
+    public class HelloWorldTest
+    {
+        [Test]
+        public void Hello_World_Quick_Start()
+        {
+            var holdings = new Examples.HelloWorld().Run();
+            Assert.That(holdings.Count, Is.EqualTo(1));
+        }
+    }
+}
