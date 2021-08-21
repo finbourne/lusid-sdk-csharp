a16fe23e7028fb8c40cce3cb0ddc2fcc1562c066
Concourse <concourse@finbourne.com>
2021-08-21 19:35:38 +0000

    Update SDK 0.11.3412 [rebase]

--- sdk/Lusid.Sdk/Client/Configuration.cs
+++ sdk/Lusid.Sdk/Client/Configuration.cs
@@ -111,7 +111,7 @@ namespace Lusid.Sdk.Client
         public Configuration()
         {
             UserAgent = "OpenAPI-Generator/0.11.3412/csharp";
-            BasePath = "http://local-unit-test-server.lusid.com:50272";
+            BasePath = "https://fbn-prd.lusid.com/api";
             DefaultHeader = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
             ApiKeyPrefix = new ConcurrentDictionary<string, string>();
@@ -127,7 +127,7 @@ namespace Lusid.Sdk.Client
             IDictionary<string, string> defaultHeader,
             IDictionary<string, string> apiKey,
             IDictionary<string, string> apiKeyPrefix,
-            string basePath = "http://local-unit-test-server.lusid.com:50272") : this()
+            string basePath = "https://fbn-prd.lusid.com/api") : this()
         {
             if (string.IsNullOrWhiteSpace(basePath))
                 throw new ArgumentException("The provided basePath is invalid.", "basePath");
