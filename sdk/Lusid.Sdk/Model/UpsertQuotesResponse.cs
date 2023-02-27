3bf7246e3a86432aeb52449f9e471e20abd21dee
Concourse <concourse@finbourne.com>
2023-02-27 21:21:03 +0000

    Update SDK 0.11.5276 [rebase]

--- sdk/Lusid.Sdk/Model/UpsertQuotesResponse.cs
+++ sdk/Lusid.Sdk/Model/UpsertQuotesResponse.cs
@@ -38,7 +38,7 @@ namespace Lusid.Sdk.Model
         /// <param name="href">The specific Uniform Resource Identifier (URI) for this resource at the requested effective and asAt datetime..</param>
         /// <param name="values">The quotes which have been successfully updated or inserted..</param>
         /// <param name="failed">The quotes that could not be updated or inserted along with a reason for their failure..</param>
-        /// <param name="links">Collection of links..</param>
+        /// <param name="links">links.</param>
         public UpsertQuotesResponse(string href = default(string), Dictionary<string, Quote> values = default(Dictionary<string, Quote>), Dictionary<string, ErrorDetail> failed = default(Dictionary<string, ErrorDetail>), List<Link> links = default(List<Link>))
         {
             this.Href = href;
@@ -69,9 +69,8 @@ namespace Lusid.Sdk.Model
         public Dictionary<string, ErrorDetail> Failed { get; set; }
 
         /// <summary>
-        /// Collection of links.
+        /// Gets or Sets Links
         /// </summary>
-        /// <value>Collection of links.</value>
         [DataMember(Name = "links", EmitDefaultValue = true)]
         public List<Link> Links { get; set; }
 
--- sdk/docs/ConstituentsAdjustmentHeader.md
+++ sdk/docs/ConstituentsAdjustmentHeader.md
@@ -6,7 +6,7 @@ Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
 **EffectiveAt** | **DateTimeOffset** | There can be at most one holdings adjustment for a portfolio at a  specific effective time so this uniquely identifies the adjustment. | [optional] 
 **Version** | [**Version**](Version.md) |  | [optional] 
-**Links** | [**List&lt;Link&gt;**](Link.md) | Collection of links. | [optional] 
+**Links** | [**List&lt;Link&gt;**](Link.md) |  | [optional] 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
 
