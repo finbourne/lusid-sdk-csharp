66b9baa8bfca98ec8e0ab10e4c12785571ddac24
Concourse <concourse@finbourne.com>
2023-06-14 18:32:13 +0000

    Update SDK 1.0.256 [rebase]

--- sdk/Lusid.Sdk/Model/CompositeDispersionResponse.cs
+++ sdk/Lusid.Sdk/Model/CompositeDispersionResponse.cs
@@ -37,7 +37,7 @@ namespace Lusid.Sdk.Model
         /// </summary>
         /// <param name="href">The specific Uniform Resource Identifier (URI) for this resource at the requested effective and asAt datetime..</param>
         /// <param name="results">Dispersion returns calculation grouped by ReturnId.</param>
-        /// <param name="links">Collection of links..</param>
+        /// <param name="links">links.</param>
         public CompositeDispersionResponse(string href = default(string), Dictionary<string, List<CompositeDispersion>> results = default(Dictionary<string, List<CompositeDispersion>>), List<Link> links = default(List<Link>))
         {
             this.Href = href;
@@ -60,9 +60,8 @@ namespace Lusid.Sdk.Model
         public Dictionary<string, List<CompositeDispersion>> Results { get; set; }
 
         /// <summary>
-        /// Collection of links.
+        /// Gets or Sets Links
         /// </summary>
-        /// <value>Collection of links.</value>
         [DataMember(Name = "links", EmitDefaultValue = true)]
         public List<Link> Links { get; set; }
 
