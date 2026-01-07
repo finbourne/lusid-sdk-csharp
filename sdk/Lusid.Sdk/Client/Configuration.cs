e97ebb3d83ae2c7736352cbcd5c4790d322df026
Concourse <concourse@finbourne.com>
2026-01-07 11:27:40 +0000

    Updates for v2.3.1444

--- sdk/Lusid.Sdk/Client/Configuration.cs
+++ sdk/Lusid.Sdk/Client/Configuration.cs
@@ -599,7 +599,7 @@ namespace Lusid.Sdk.Client
             string report = "C# SDK (Lusid.Sdk) Debug Report:\n";
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
-            report += "    Version of the API: 0.11.8680\n";
+            report += "    Version of the API: 0.11.8682\n";
             report += "    SDK Package Version: 2.0.0\n";
 
             return report;
